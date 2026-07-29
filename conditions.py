ce827f97655cc96d725d95318bcb971be9fff315
Hassnain-Ali236 <alihassnain.ah007@gmail.com>
2026-07-29 15:14:26 +0500

    Python Basic code

---
+++ conditions.py
@@ -0,0 +1,23 @@
+# n=float(input("Enter your Number "))
+# if(n%2==0):
+#     print("Number is even")
+# else:
+#     print("Number is odd")
+#  Task -2
+# g1 = float (input("Enter your First Number"))
+# g2=  float (input("Enter your Second Number"))
+# g3=  float (input("Enter your third Number"))
+
+# if(g1 > g2 and g1 >g3):
+#     print(g1," is Greater than Other Numbers")
+# elif(g2 > g1 and g2 > g3):
+#     print(g2, "is Greater Than Other Numbers")
+# else:
+#     print(g3, "is Greater Than Other Numbers") 
+#  Tasl 03
+n = float (input ("Enter your Number"))
+m =7
+if(n%m==0):
+    print("Number can be Multiplied by 7 ")
+else:
+           print("Not * by 7")
