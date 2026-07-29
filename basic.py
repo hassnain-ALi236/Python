67197bda640777654c3e26217095c75092d30d4e
Hassnain-Ali236 <alihassnain.ah007@gmail.com>
2026-07-29 12:04:41 +0500

    first python code

---
+++ basic.py
@@ -0,0 +1,23 @@
+    #    TASK 01
+print ("My Name Is Ali Hassnain")
+name=input("Enter Your Name")
+print ("your Name is",name )
+    #  // result /sum/answer always come in string thats why typecasting is very imp in python
+                #   Task 02
+a=  float(input("Enter One Number")) 
+b = float(input("Enter Second Number"))
+sum=a+b
+print ("You Numbers Sum is", sum)
+            #  Task 03
+c = float (input("Enter width/height of an Area"))
+Area = c**2
+    #   Task 04
+print("Sqaure of the Area =" ,Area)
+first =int(input("Enter First Number"))
+Second =int(input("Enter Second Number"))
+average= first+Second
+print ("Avreage is :",average/2)
+            # Task 05
+task5_1= float(input("Enter First Number"))
+task5_2 =float (input("Enter Second Number"))
+print (task5_1 >= task5_2)
