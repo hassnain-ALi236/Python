6297d55960c08248a436ce5a3f7134b0a8887b82
Hassnain-Ali236 <alihassnain.ah007@gmail.com>
2026-08-20 20:35:01 +0500

    Add new Code

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,6 @@
-even_count = 0
-odd_count = 0
-for i in range(1, 11):
-    num = int(input(f"Number {i} enter karein: "))
-    if num % 2 == 0:
-        even_count += 1
-    else:
-        odd_count += 1
-print(f"Total Even Numbers: {even_count}")
-print(f"Total Odd Numbers:  {odd_count}")
+class car:
+    @staticmethod
+    def color():
+        print("Car Colour is Blue :")
+c1 = car()
+print(c1.color())
