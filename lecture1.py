6b95d3da9b448d815ba73f7794e32d241f70b4d7
Hassnain-Ali236 <alihassnain.ah007@gmail.com>
2026-08-24 14:08:41 +0500

    remove comments

--- lecture1.py
+++ lecture1.py
@@ -1,19 +1,19 @@
-# str= "Ali\tHassnain"
-# print(str)
-# print(str[5])
-# print(len(str))
-# # str[5]= "s" we cant set string indexes like other languages JS etc
-# str1= "Ali"
-# str2="Hassnain"
-# final_string= str1+str2
-# print(final_string)
-#         #  Slicing/\
-# Name= "Umair Javed"
-# # this is called slicing
-# print (Name[1 : 3])
-# fruit = "Apple"
-# # negative indexing is only applicable in python 
-# print(fruit[-3:-1])
+str= "Ali\tHassnain"
+print(str)
+print(str[5])
+print(len(str))
+# str[5]= "s" we cant set string indexes like other languages JS etc
+str1= "Ali"
+str2="Hassnain"
+final_string= str1+str2
+print(final_string)
+        #  Slicing/\
+Name= "Umair Javed"
+# this is called slicing
+print (Name[1 : 3])
+fruit = "Apple"
+# negative indexing is only applicable in python 
+print(fruit[-3:-1])
 name= "ali hassnain"
 print(name.endswith("in"))
 print(name.capitalize())
