6b1ab12f1318e9bda6ff698390fe6713903498b7
Hassnain-Ali236 <alihassnain.ah007@gmail.com>
2026-08-28 21:11:04 +0500

    Change to latest code

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,11 @@
-class car:
-    @staticmethod
-    def color():
-        print("Car Colour is Blue :")
-c1 = car()
-print(c1.color())
+n1=int(input("Please Enter One Number : "))
+n2=int(input("Please Enter Second Number : "))
+operater=input("Select an Operator (+-*/) : ")
+if(operater == "+"):
+    print (f"Result =", n1 + n2)
+elif(operater== "-"):
+    print(f"Result =", n1 - n2)
+elif(operater== "*"):
+    print(f"Result =", n1 * n2)
+else:
+    print(f"Result =", n1/n2)
